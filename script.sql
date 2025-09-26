febe2151dd508565af47dc9a36a8b09e3fe3dadd
Kassime <134729407+GiKassime@users.noreply.github.com>
2025-09-25 21:23:51 -0300

    Update script.sql
    Script bdd mais atualizado

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 14/09/2025 às 17:14
+-- Tempo de geração: 26/09/2025 às 00:22
 -- Versão do servidor: 8.3.0
 -- Versão do PHP: 8.3.6
 
@@ -30,8 +30,8 @@ CREATE database pobrelist;
 use pobrelist;
 CREATE TABLE `categoria` (
   `id` int NOT NULL,
-  `nome` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `categoria_cor` varchar(7) COLLATE utf8mb4_general_ci NOT NULL
+  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `categoria_cor` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -42,7 +42,7 @@ INSERT INTO `categoria` (`id`, `nome`, `categoria_cor`) VALUES
 (1, 'Limpeza', '#528bff'),
 (2, 'Tecnologia', '#1E90FF'),
 (4, 'Casa & Decoração', '#32CD32'),
-(6, 'Livros', '#8A2BE2'),
+(9, 'Lazer', '#ff004c'),
 (7, 'Gatos', '#ff00d0'),
 (8, 'Acessórios', '#9478d3');
 
@@ -54,13 +54,13 @@ INSERT INTO `categoria` (`id`, `nome`, `categoria_cor`) VALUES
 
 CREATE TABLE `item` (
   `id` int NOT NULL,
-  `nome` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
-  `descricao` text COLLATE utf8mb4_general_ci NOT NULL,
+  `nome` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `descricao` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `categoria_id` int NOT NULL,
   `prioridade_id` int NOT NULL,
   `preco_estimado` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `url_imagem` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `url_web` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `url_imagem` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `url_web` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `data_desejo` date NOT NULL,
   `comprado` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -71,13 +71,18 @@ CREATE TABLE `item` (
 
 INSERT INTO `item` (`id`, `nome`, `descricao`, `categoria_id`, `prioridade_id`, `preco_estimado`, `url_imagem`, `url_web`, `data_desejo`, `comprado`) VALUES
 (22, 'Mop Fácil Dobrável', 'Para facilitar a limpeza diária', 1, 13, 32.30, 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lr82zgztoqas5c.webp', 'https://s.shopee.com.br/Ldq3pv3IG', '2025-10-06', 0),
-(23, 'Pintura da parede do quarto', 'mão de obra e produtos', 4, 14, 400.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTffrphqgwwpe7vbdzirrlcKhsp5ybxlizOVg&s', NULL, '2025-09-22', 0),
-(24, 'Escova A Vapor Para Gatos', 'Os gatos soltam muito pelo mds', 7, 13, 11.89, 'https://down-br.img.susercontent.com/file/sg-11134201-7req9-m8m4i8ulopef66.webp', 'https://s.shopee.com.br/AUkcApwj7N', '2025-10-06', 0),
+(23, 'Pintura da parede do quarto', 'mão de obra e produtos', 4, 14, 300.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTffrphqgwwpe7vbdzirrlcKhsp5ybxlizOVg&s', NULL, '2025-09-22', 1),
+(24, 'Escova A Vapor Para Gatos', 'Os gatos soltam muito pelo mds', 7, 13, 11.89, 'https://down-br.img.susercontent.com/file/sg-11134201-7req9-m8m4i8ulopef66.webp', 'https://s.shopee.com.br/AUkcApwj7N', '2025-10-06', 1),
 (17, 'Tapete Arranhador Para Gatos', 'Pros gatos pararem de destruir minha cama', 7, 5, 15.99, 'https://down-br.img.susercontent.com/file/sg-11134201-7rce3-m6czqqeb5lz4d9.webp', 'https://s.shopee.com.br/6fXtaDkqo3', '2025-09-30', 0),
 (20, 'Kit 02 Travesseiros', 'Travesseiros', 4, 14, 33.90, 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lqnrhbjpwkdj15.webp', 'https://s.shopee.com.br/8pcOB6tEdm', '2025-10-06', 0),
-(18, 'Luminária Pendente', 'Para substituir a luminária que quebrou', 4, 12, 49.90, 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-mcwmfm0ml5bmbb.webp', 'https://s.shopee.com.br/803HAzg9SN', '2025-10-06', 0),
+(18, 'Luminária Pendente', 'Para substituir a luminária que quebrou', 4, 12, 58.90, 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-mcwmfm0ml5bmbb.webp', 'https://s.shopee.com.br/803HAzg9SN', '2025-10-06', 1),
 (19, 'Rodo De Limpeza Dobrável', 'Necessário para facilitar a limpeza', 1, 13, 18.00, 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lxtmgjoyxth2f4.webp', 'https://s.shopee.com.br/5ffMOrC8ns', '2025-10-05', 0),
-(21, 'Breu Violino', 'Acabou o meu breu', 8, 14, 20.00, 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lzflw8ruodglf2.webp', NULL, '2025-09-23', 1);
+(21, 'Breu Violino', 'Acabou o meu breu', 8, 14, 20.00, 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lzflw8ruodglf2.webp', NULL, '2025-09-23', 1),
+(25, 'Placa Display Completa Ar', 'Para o ar ligar', 4, 14, 225.00, 'https://http2.mlstatic.com/D_NQ_NP_2X_914020-MLB89124705283_082025-F-placa-display-completa-ar-condicionado-lg-inverter-9000.webp', NULL, '2025-09-21', 1),
+(26, 'Limpeza e manutenção do ar', 'Precisa limpar e resolver o caninho', 4, 14, 150.00, 'https://lojaartech.cdn.magazord.com.br/img/2024/09/produto/17571/evaporadora-da-condensadora-eco-dream-inverter-wi-fi-9000-btus-quente-e-fria-elgin-r32-classe-a-inmetro-artech-climatizacao-hiqi09c2wa-01-1.png', NULL, '2025-10-05', 1),
+(27, 'Show do Linkin Park', 'Show no Zepellin', 9, 13, 66.00, 'https://images.sympla.com.br/68a28014a96bf-lg.jpg', NULL, '2025-10-24', 1),
+(28, 'Mesa 200x60cm', 'Mesa pro quarto', 4, 5, 500.00, 'https://http2.mlstatic.com/D_NQ_NP_2X_916838-MLB86018605870_062025-F-mesa-200x60-para-escritorio-escrivaninha-cnt30.webp', 'https://produto.mercadolivre.com.br/MLB-1481034639-mesa-200x60-para-escritorio-escrivaninha-cnt30-_JM?searchVariation=173992526984#polycard_client=bookmarks', '2025-10-31', 0),
+(29, '2 Cadeiras de Escritório', 'Para a mesa', 4, 13, 800.00, 'https://abracasa.vteximg.com.br/arquivos/ids/181718/cadeira-de-escritorio-franca-preto-diagonal.jpg?v=637967860223230000', NULL, '2025-10-31', 0);
 
 -- --------------------------------------------------------
 
@@ -87,8 +92,8 @@ INSERT INTO `item` (`id`, `nome`, `descricao`, `categoria_id`, `prioridade_id`, 
 
 CREATE TABLE `prioridade` (
   `id` int NOT NULL,
-  `nivel` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `prioridade_cor` varchar(7) COLLATE utf8mb4_general_ci NOT NULL
+  `nivel` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `prioridade_cor` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -134,13 +139,13 @@ ALTER TABLE `prioridade`
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de tabela `prioridade`
